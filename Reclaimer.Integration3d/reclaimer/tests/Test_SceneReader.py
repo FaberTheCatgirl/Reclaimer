4c6c39a5acd4ba78df393da7e3985311fbcfedbc
Gravemind2401 <inferi_sententia@hotmail.com>
2023-02-04 18:58:24 +1100

    type aliases for vectors

--- Reclaimer.Integration3d/reclaimer/tests/Test_SceneReader.py
+++ Reclaimer.Integration3d/reclaimer/tests/Test_SceneReader.py
@@ -1,10 +1,20 @@
 import unittest
 from ..src.SceneReader import SceneReader
 
-class Test_SceneReader(unittest.TestCase):
-    def test_test(self):
+class Test_Citadel(unittest.TestCase):
+    def test_citadel(self):
         sr = SceneReader()
         sr.read_scene('C:\\Users\\Rhys\\Desktop\\100_citadel.rmf')
+
+class Test_Brute(unittest.TestCase):
+    def test_brute(self):
+        sr = SceneReader()
+        sr.read_scene('C:\\Users\\Rhys\\Desktop\\brute.rmf')
+
+class Test_Masterchief(unittest.TestCase):
+    def test_masterchief(self):
+        sr = SceneReader()
+        sr.read_scene('C:\\Users\\Rhys\\Desktop\\masterchief.rmf')
 
 if __name__ == '__main__':
     unittest.main()
