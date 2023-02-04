dc622b2be68a891a8316640119aa9ee83ac88f2b
Gravemind2401 <inferi_sententia@hotmail.com>
2023-02-04 22:09:21 +1100

    more reading

--- Reclaimer.Integration3d/reclaimer/tests/Test_SceneReader.py
+++ Reclaimer.Integration3d/reclaimer/tests/Test_SceneReader.py
@@ -3,18 +3,18 @@ from ..src.SceneReader import SceneReader
 
 class Test_Citadel(unittest.TestCase):
     def test_citadel(self):
-        sr = SceneReader()
-        sr.read_scene('C:\\Users\\Rhys\\Desktop\\100_citadel.rmf')
+        scene = SceneReader.open_scene('C:\\Users\\Rhys\\Desktop\\100_citadel.rmf')
+        return
 
 class Test_Brute(unittest.TestCase):
     def test_brute(self):
-        sr = SceneReader()
-        sr.read_scene('C:\\Users\\Rhys\\Desktop\\brute.rmf')
+        scene = SceneReader.open_scene('C:\\Users\\Rhys\\Desktop\\brute.rmf')
+        return
 
 class Test_Masterchief(unittest.TestCase):
     def test_masterchief(self):
-        sr = SceneReader()
-        sr.read_scene('C:\\Users\\Rhys\\Desktop\\masterchief.rmf')
+        scene = SceneReader.open_scene('C:\\Users\\Rhys\\Desktop\\masterchief.rmf')
+        return
 
 if __name__ == '__main__':
     unittest.main()
