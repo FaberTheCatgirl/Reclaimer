ef5b04dba2221335d18ae61676b6a9f0e06b1a4e
Gravemind2401 <inferi_sententia@hotmail.com>
2023-02-25 19:20:31 +1100

    more pyside2

--- Reclaimer.Integration3d/reclaimer/UI/RmfWidget.py
+++ Reclaimer.Integration3d/reclaimer/UI/RmfWidget.py
@@ -1,4 +1,6 @@
-from typing import Union
+from typing import Any, Union
+from typing import cast
+
 from PySide2 import QtCore
 from PySide2 import QtWidgets
 from PySide2.QtWidgets import QWidget, QTreeWidget, QTreeWidgetItem
@@ -8,36 +10,90 @@ from ..src import Scene, Model
 CheckState = QtCore.Qt.CheckState
 SceneItem = Union[Scene.SceneGroup, Model.Model, Model.ModelRegion, Model.ModelPermutation]
 
+_typeDisplayNames = {
+    Scene.SceneGroup: 'Group',
+    Model.Model: 'Model',
+    Model.ModelRegion: 'Region',
+    Model.ModelPermutation: 'Permutation'
+}
+
 class RmfTreeItem(QTreeWidgetItem):
     def __init__(self, parent: Union[QTreeWidget, 'RmfTreeItem'], item: SceneItem):
         super(RmfTreeItem, self).__init__(parent)
         self.setCheckState(0, CheckState.Checked)
         self.setText(0, str(item))
-        self.setText(1, type(item).__name__)
+        self.setText(1, _typeDisplayNames[type(item)])
+
+    def setData(self, column: int, role: int, value: Any):
+        super(RmfTreeItem, self).setData(column, role, value)
+        if column == 0 and role == QtCore.Qt.CheckStateRole:
+            self._refresh()
+
+    def _refresh(self):
+        hideChildren = self.checkState(0) == CheckState.Unchecked
+        for i in range(self.childCount()):
+            c = cast(RmfTreeItem, self.child(i))
+            c.setHidden(hideChildren)
+            if not hideChildren:
+                c._refresh()
 
 
 class RmfWidget(QWidget):
     _scene: Scene.Scene
+    _treeview: QTreeWidget
 
     def __init__(self, parent: QWidget, scene: Scene.Scene):
         super(RmfWidget, self).__init__(parent)
         self._scene = scene
-        layout = QtWidgets.QGridLayout()
+
+        treePanel = QtWidgets.QVBoxLayout()
+        toolbar = self._create_toolbar()
+        treeView = self._create_treeview()
         
-        treeView = QTreeWidget()
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(treeView)
+
+        treePanel.addWidget(toolbar)
+        treePanel.addLayout(layout, 1)
+
+        footer = self._create_footer()
+        treePanel.addLayout(footer, 0)
+
+        self.setLayout(treePanel)
+
+    def _create_toolbar(self):
+        toolbar = QtWidgets.QToolBar()
+
+        expNone = QtWidgets.QPushButton('[-]')
+        expNone.clicked.connect(lambda: self._treeview.collapseAll())
+        toolbar.addWidget(expNone)
+
+        expAll = QtWidgets.QPushButton('[+]')
+        expAll.clicked.connect(lambda: self._treeview.expandAll())
+        toolbar.addWidget(expAll)
+
+        return toolbar
+
+    def _create_footer(self):
+        panel = QtWidgets.QVBoxLayout()
+        button = QtWidgets.QPushButton('Import')
+        panel.addWidget(button, 0, QtCore.Qt.AlignHCenter)
+        return panel
+
+    def _create_treeview(self):
+        treeView = self._treeview = QTreeWidget()
+
         treeView.setColumnCount(2)
         treeView.setAlternatingRowColors(True)
         treeView.setHeaderLabels(['Name', 'Type'])
 
-        layout.addWidget(treeView)
-
-        treeView.addTopLevelItems([self._build_group(treeView, g) for g in scene.root_node.child_groups])
+        treeView.addTopLevelItems([self._build_group(treeView, g) for g in self._scene.root_node.child_groups])
         treeView.expandAll()
         treeView.resizeColumnToContents(0)
         treeView.resizeColumnToContents(1)
         treeView.adjustSize()
-
-        self.setLayout(layout)
+        
+        return treeView
 
     def _build_group(self, parent: Union[QTreeWidget, 'RmfTreeItem'], group: Scene.SceneGroup) -> RmfTreeItem:
         item = RmfTreeItem(parent, group)
